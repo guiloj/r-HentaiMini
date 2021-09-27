aa86adb28ce2f7ff3a92e12a5446df220e7a92cc
guiloj <61667899+guiloj@users.noreply.github.com>
2021-09-27 17:06:34 -0300

    added abspath @IllyaSempai and fixed win32 support
    - `os.path.abspath(__file__)` fix added by @IllyaSempai
    - windows support fixed with curl
    - images also get names with numbers now

--- scripts/getimgs.py
+++ scripts/getimgs.py
@@ -1,7 +1,8 @@
 import os
 import requests
 
-os.chdir(os.path.dirname(__file__))
+absFilePath = os.path.abspath(__file__)
+os.chdir( os.path.dirname(absFilePath) )
 filesep = "\\" if os.name == "nt" else "/"
 if not os.path.isdir("files") or not os.path.isfile(f"files{filesep}imgs.txt"):
     print(
@@ -9,33 +10,28 @@ if not os.path.isdir("files") or not os.path.isfile(f"files{filesep}imgs.txt"):
     )
     quit()
 try:
-    os.mkdir(os.path.join(os.path.dirname(__file__), "imgs"))
+    os.mkdir(os.path.join(os.path.dirname(absFilePath), "imgs"))
 except OSError as error:
     print(error)
-    path = os.path.join(os.path.dirname(__file__), "imgs")
+    path = os.path.join(os.path.dirname(absFilePath), "imgs")
     print(
         f'if the "{path}" directory already exists in this path please remove it or move the file'
     )
     quit()
 with open(f"files{filesep}imgs.txt", "rt", encoding="utf-8") as f:
     content = f.readlines()
-os.chdir(os.path.dirname(__file__) + filesep + "imgs")
-indx = 0
-for x in content:
-    if str(x) == "0":
+os.chdir(os.path.dirname(absFilePath) + filesep + "imgs")
+
+for x, y in zip(content, range(len(content))):
+    if "0" in str(x):
         continue
-    if os.name != "nt":
-        os.system(f"wget {x}")
+    for typ in (".jpg", ".png", ".gif", ".jpeg"):
+        if typ in x:
+            mtype = typ
+            break
     else:
-        with open(f"pic{indx}.jpg", "wb") as handle:
-            response = requests.get(x, stream=True)
-
-            if not response.ok:
-                print(response)
-
-            for block in response.iter_content(1024):
-                if not block:
-                    break
-
-                handle.write(block)
-        indx += 1
+        mtype = '.jpg'
+    if os.name != 'nt':
+        os.system(f"wget -O img{y}{mtype} {x}")
+    else:
+        os.system(f"curl -o img{y}{mtype} {x}")
