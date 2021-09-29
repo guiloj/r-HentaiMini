37bfb4e92e7e59c8236cb946ddbcf402bf946249
guiloj <61667899+guiloj@users.noreply.github.com>
2021-09-29 10:38:51 -0300

    now more statically typed

--- scripts/getimgs.py
+++ scripts/getimgs.py
@@ -1,8 +1,10 @@
 import os
 
-absFilePath = os.path.abspath(__file__)
-os.chdir( os.path.dirname(absFilePath) )
-filesep = "\\" if os.name == "nt" else "/"
+absFilePath: str = os.path.abspath(__file__)
+os.chdir(os.path.dirname(absFilePath))
+
+filesep: str = "\\" if os.name == "nt" else "/"
+
 if not os.path.isdir("files") or not os.path.isfile(f"files{filesep}imgs.txt"):
     print(
         'the "files" directory was not found or is incomplete, please move this file to the correct directory or run "hentaimini.py" again'
@@ -12,13 +14,13 @@ try:
     os.mkdir(os.path.join(os.path.dirname(absFilePath), "imgs"))
 except OSError as error:
     print(error)
-    path = os.path.join(os.path.dirname(absFilePath), "imgs")
+    path: str = os.path.join(os.path.dirname(absFilePath), "imgs")
     print(
         f'if the "{path}" directory already exists in this path please remove it or move the file'
     )
     quit()
 with open(f"files{filesep}imgs.txt", "rt", encoding="utf-8") as f:
-    content = f.readlines()
+    content: list = f.readlines()
 os.chdir(os.path.dirname(absFilePath) + filesep + "imgs")
 
 for x, y in zip(content, range(len(content))):
@@ -26,11 +28,12 @@ for x, y in zip(content, range(len(content))):
         continue
     for typ in (".jpg", ".png", ".gif", ".jpeg"):
         if typ in x:
-            mtype = typ
+            mtype: str = typ
             break
     else:
-        mtype = '.jpg'
-    if os.name != 'nt':
+        mtype: str = ".jpg"
+    if os.name != "nt":
         os.system(f"wget -O img{y}{mtype} {x}")
     else:
         os.system(f"curl -o img{y}{mtype} {x}")
+print("DONE!")
